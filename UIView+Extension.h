88d29086c976add9f85bd26ad306a9be25b653b2
Ellmcz <wqbs007@163.com>
2016-08-01 02:06:43 +0800

    提交框架

---
+++ UIView+Extension.h
@@ -0,0 +1,20 @@
+//
+//  
+//  GroupBuyFoodHD
+//
+//  Created by 王钱宝 on 16/2/23.
+//  Copyright © 2016年 ellmcz. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat maxX;
+@property (nonatomic, assign) CGFloat maxY;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@end
